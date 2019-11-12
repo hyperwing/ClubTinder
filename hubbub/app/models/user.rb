5fb3b2a844721a7ffa3ed0e8354021ce558867b3
sriramya <sriramyadandu>
2019-11-12 18:00:57 -0500

    validations

--- hubbub/app/models/user.rb
+++ hubbub/app/models/user.rb
@@ -1,6 +1,8 @@
 # Created 11/09/2019 by Sri Ramya Dandu
 # Edited 11/10/2019 by Neel Mansukhani: Added admin role.
+# Edited 11/12/2019 by Sri Ramya Dandu: Added validation for grad year
 class User < ApplicationRecord
+  validates :grad_year, length: {maximum: 4, minimum: 4}
   enum role: [:user, :club, :admin]
   after_initialize :set_default_role, :if => :new_record?
   def set_default_role
