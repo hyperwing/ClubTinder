d8d6ee8f26422e31e220cf650fc1600f03ea22e7
sharonqiu1 <sharonqiu1@users.noreply.github.com>
2019-11-13 16:32:06 -0500

    quick syntax change in club model.

--- hubbub/app/models/club.rb
+++ hubbub/app/models/club.rb
@@ -2,5 +2,5 @@
 # Edited 11/12/2019 by Neel Mansukhani: Added relations
 class Club < ApplicationRecord
     has_many :club_matches
-    has_many :clubs, through: :club_matches
+    has_many :users, through: :club_matches
 end
