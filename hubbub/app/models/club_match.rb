a0c19136293fa46bfde64fcf7321a26f155b9efa
neelm88 <neelm88@gmail.com>
2019-11-12 19:07:38 -0500

    Create suggested movie algorithm

--- hubbub/app/models/club_match.rb
+++ hubbub/app/models/club_match.rb
@@ -1,2 +1,6 @@
+
+# Edited 11/12/2019 by Neel Mansukhani: Added relations
 class ClubMatch < ApplicationRecord
+    belongs_to :user
+    belongs_to :movie
 end
--- hubbub/app/models/user.rb
+++ hubbub/app/models/user.rb
@@ -1,10 +1,33 @@
 # Created 11/09/2019 by Sri Ramya Dandu
 # Edited 11/10/2019 by Neel Mansukhani: Added admin role.
+# Edited 11/12/2019 by Neel Mansukhani: Added relations
+
 class User < ApplicationRecord
+  has_many :club_matches
+  has_many :clubs, through: :club_matches
+
   enum role: [:user, :club, :admin]
   after_initialize :set_default_role, :if => :new_record?
+
+  # Created 11/10/2019 by Neel Mansukhani
+  # Sets default role to user.
   def set_default_role
     self.role ||= :user
+  end
+
+  # Created 11/12/2019 by Neel Mansukhani
+  # Returns all clubs ordered by how likely a user is to like this club.
+  def ordered_clubs
+    other_users = self.class.all.where.not id: self.id
+    clubs = Hash.new 0
+    other_users.each do |user|
+      common_clubs = user.clubs & self.clubs
+      weight = common_clubs.length.to_f / user.clubs.length
+      (user.clubs — common_clubs).each do |club|
+        clubs[club] += weight
+      end
+    end
+    sorted = clubs.sort_by{ |k, v| v }.reverse
   end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
