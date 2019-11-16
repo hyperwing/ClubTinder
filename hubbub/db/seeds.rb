2240389b44ea8e218223c3ad62f631d9109f8321
neelm88 <neelm88@gmail.com>
2019-11-16 16:37:55 -0500

    seed fix

--- hubbub/db/seeds.rb
+++ hubbub/db/seeds.rb
@@ -1,5 +1,5 @@
 # Edited 11/14/2019 by Leah Gillespie
-
+# Edited 11/16/2019 by Neel Mansukhani: Seed fix
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,10 +7,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Club.destroy_all
 data_file = File.open '../data-scrape/clubs.txt', 'r'
 data_string = data_file.read
 data = eval data_string
 data.each do |c, d|
-  Club.create name: d[:name], mission: d[:mission], link: d[:link], location: d[:location], affiliations: d[:affiliations], service: d[:service]
+  x = Club.create name: d[:name], mission: d[:mission], link: d[:link], location: d[:location], affiliations: d[:affiliations], service: d[:service]
+  x.save
 end
