f2de7e78722ea907979d5bacc9012a7f5ea2c30f
neelm88 <neelm88@gmail.com>
2019-11-16 23:59:41 -0500

    removed interest controller tests

--- hubbub/test/controllers/interests_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class InterestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get interests_new_url
-    assert_response :success
-  end
-
-end
