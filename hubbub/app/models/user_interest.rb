6bae1c11c4496604913bf3d97f2bf52ef3d7ccd3
david wing <drdavidwing@gmail.com>
2019-11-18 10:34:55 -0500

    added scaffold tests to user_interests

--- hubbub/app/models/user_interest.rb
+++ hubbub/app/models/user_interest.rb
@@ -1,2 +1,6 @@
+# Created 11/15/19 David Wing
+# Edited 11/17/19 David Wing
 class UserInterest < ApplicationRecord
+    belongs_to :user, foreign_key: 'user_id'
+    belongs_to :interest, foreign_key: 'id'
 end
--- hubbub/test/controllers/user_interests_controller_test.rb
+++ hubbub/test/controllers/user_interests_controller_test.rb
@@ -1,16 +1,50 @@
+# Created 11/18/2019 by David Wing 
+
 require 'test_helper'
 
 class UserInterestsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_interest = user_interests(:one)
+  end
 
-  # test "should get stats" do
-  #   get users_stats_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get user_interests_url
+    assert_response :success
+  end
 
+  test "should get new" do
+    get new_user_interest_url
+    assert_response :success
+  end
 
+  test "should create user_interest" do
+    assert_difference('UserInterest.count') do
+      post user_interests_url, params: { user_interest: {  } }
+    end
 
+    assert_redirected_to user_interest_url(UserInterest.last)
+  end
 
+  test "should show user_interest" do
+    get user_interest_url(@user_interest)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_user_interest_url(@user_interest)
+    assert_response :success
+  end
+
+  test "should update user_interest" do
+    patch user_interest_url(@user_interest), params: { user_interest: {  } }
+    assert_redirected_to user_interest_url(@user_interest)
+  end
+
+  test "should destroy user_interest" do
+    assert_difference('UserInterest.count', -1) do
+      delete user_interest_url(@user_interest)
+    end
+
+    assert_redirected_to user_interests_url
+  end
 end
