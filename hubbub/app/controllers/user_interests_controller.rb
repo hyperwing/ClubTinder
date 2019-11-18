33cb446bc3df751ad6c4bad66e42aa2ba0b8b888
david wing <drdavidwing@gmail.com>
2019-11-17 20:33:09 -0500

    user interests view updated to reflect checks and grabs current user info

--- hubbub/app/controllers/user_interests_controller.rb
+++ hubbub/app/controllers/user_interests_controller.rb
@@ -3,6 +3,9 @@
 class UserInterestsController < ApplicationController
     def select_user_interests
         @interests = Interest.all
+        # current user interests
+        @user_interests = UserInterest.where(:user_id== current_user.id)
 
+        @cur_u = current_user
     end
 end
