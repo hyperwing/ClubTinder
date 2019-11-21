9306b88c80f91238191cbcae942dc8de95ba6b02
sriramya <sriramyadandu>
2019-11-20 22:58:57 -0500

    Test work, added more tests for user vs. admin routes

--- hubbub/test/controllers/users_controller_test.rb
+++ hubbub/test/controllers/users_controller_test.rb
@@ -1,7 +1,71 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users(:admin)
+  end
 
+  test "redirected from index if not logged in" do
+    sign_out :user
+    get users_url
+    assert_response :redirect
+  end
+
+  test "admin can access index" do
+    sign_in users(:admin)
+    get users_url
+    assert_response :success
+  end
+
+  test "redirected stats if not logged in" do
+    sign_out :user
+    get users_stats_url
+    assert_response :redirect
+  end
+
+  test "redirected new if not logged in" do
+    sign_out :user
+    get users_new_url
+    assert_response :redirect
+  end
+
+  test "redirected display_stats if not logged in" do
+    sign_out :user
+    get users_display_stats_url
+    assert_response :redirect
+  end
+
+  test "user can't access index" do
+    sign_in users(:user)
+    get users_url
+    assert_response :redirect
+  end
+
+  test "user can't access stats " do
+    sign_in users(:user)
+    get users_stats_url
+    assert_response :redirect
+  end
+
+  test "user can't create new user" do
+    sign_in users(:user)
+    get users_new_url
+    assert_response :redirect
+  end
+
+  test "user can't access display_stats" do
+    sign_in users(:user)
+    get users_display_stats_url
+    assert_response :redirect
+  end
+
+  test "user can't create dummy user" do
+    sign_in users(:user)
+    get users_create_url
+    assert_response :redirect
+  end
+  
+  
   test "Should not save user without email" do
     user = User.new
     assert_not user.save, "Saved the user without an email"
@@ -11,6 +75,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     post = User.new(email:"sri@yahoo.com")
     assert_not post.save
   end
+
 
   test "should get stats" do
     get users_stats_url
@@ -22,4 +87,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test 'anyone can access explore' do
+    sign_out :user
+    get users_explore_url
+    assert_response :success
+  end
+
+  test "admin can create dummy user" do
+    get users_create_url
+    assert_response :success
+  end
 end
