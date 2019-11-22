1a997a6a31ed06f5286d00aa133c7928d44bd23f
david wing <drdavidwing@gmail.com>
2019-11-22 02:06:08 -0500

    additional user stat graphs/reformatting

--- hubbub/app/controllers/users_controller.rb
+++ hubbub/app/controllers/users_controller.rb
@@ -159,6 +159,7 @@ class UsersController < ApplicationController
       @keysArray = params.keys[0,(params.keys).length-2]
       @match_data = ClubMatch.joins(:club).where(:matched=>1).order('COUNT(club_matches.club_id)DESC').limit(5)
       @user_interest_data = UserInterest.joins(:interest)
+      @club_interest_data = ClubInterest.joins(:interest)
     
       if @valuesArray[0] != "All" 
         @users = User.where("#{@keysArray[0]}": params["#{@keysArray[0]}".to_sym])
