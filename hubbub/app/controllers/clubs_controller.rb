247e244f7845d759a2aef2f316045487372ff309
david wing <drdavidwing@gmail.com>
2019-11-22 12:25:44 -0500

    my club stat page update

--- hubbub/app/controllers/clubs_controller.rb
+++ hubbub/app/controllers/clubs_controller.rb
@@ -129,6 +129,8 @@ class ClubsController < ApplicationController
       @user_interest_data = UserInterest.left_joins(:interest).where(user_id: @club.users).group(:name).limit(5).order('COUNT(interests.id) DESC').count
       @gender_data = ClubMatch.left_joins(:user).where(club_id: @club.id).group(:gender).count
 
+      @club_interests = ClubInterest.left_joins(:interest).where(club_id: current_user.club_id)
+
       @club_has_matches = ClubMatch.where(club_id: @club.id).count >0
     
       
