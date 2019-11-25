da56649f82ec09552178f3f76b10858891de6724
sriramya <sriramyadandu>
2019-11-25 18:02:03 -0500

    Added model test

--- hubbub/test/models/user_test.rb
+++ hubbub/test/models/user_test.rb
@@ -1,7 +1,54 @@
+# Edited 11/25/2019 by Sri Ramya Dandu
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+      @user = users(:user)
+      @admin = users(:admin)
+  end
+
+  test "user should not save without any attributes" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "user should not save without email" do
+    user = User.new
+    user.first_name = "Sri"
+    user.last_name= "Dandu"
+    user.gender = "Female"
+    user.grad_year= "2021"
+    user.role = "user"
+    assert_not user.save
+  end
+
+  test "user should not save without first name" do
+    user = User.new
+    user.email = "dandu.7@osu.edu"
+    user.last_name= "Dandu"
+    user.gender = "Female"
+    user.grad_year= "2021"
+    user.role = "user"
+    assert_not user.save
+  end
+
+  test "user should not save without last name" do
+    user = User.new
+    user.email = "dandu.7@osu.edu"
+    user.first_name = "Sri"
+    user.gender = "Female"
+    user.grad_year= "2021"
+    user.role = "user"
+    assert_not user.save
+  end
+
+  test "user should save with all required attributes filled in" do
+    assert @user.save
+  end
+
+  test "admin should save with all required attributes filled in" do
+    assert @admin.save
+  end
+
 end
