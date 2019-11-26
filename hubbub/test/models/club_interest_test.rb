d1deb9515ca77f3ccb0cfc76125873fe4197e527
neelm88 <neelm88@gmail.com>
2019-11-26 17:32:24 -0500

    added tests

--- hubbub/test/models/club_interest_test.rb
+++ hubbub/test/models/club_interest_test.rb
@@ -1,7 +1,21 @@
+# Edited 11/26/2019 by Neel Mansukhani
 require 'test_helper'
 
 class ClubInterestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "club interest shouldn't be valid without any information" do
+    club_interest = ClubInterest.new
+    assert_not club_interest.valid?
+  end
+
+  test "club interest shouldn't be valid without club id" do
+    club_interest = ClubInterest.new
+    club_interest.interest_id = 1;
+    assert_not club_interest.valid?
+  end
+
+  test "club interest shouldn't be valid without interest id" do
+    club_interest = ClubInterest.new
+    club_interest.club_id = 1;
+    assert_not club_interest.valid?
+  end
 end
