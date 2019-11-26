e8a4038a9cb66aa5822c3a6b58cc3a3dd0cfe29f
leahgillespie <gillespie.267@osu.edu>
2019-11-26 15:02:05 -0500

    added club model tests

--- hubbub/test/models/club_test.rb
+++ hubbub/test/models/club_test.rb
@@ -1,7 +1,26 @@
+# Edited 11/26/2019 by Leah Gillespie
+
 require 'test_helper'
 
 class ClubTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Club should save when created" do
+    club = Club.new
+    club.name = "test club"
+    club.mission = "test mission"
+    club.link = "www.osu.edu"
+    club.location = "columbus"
+    club.affiliations = "Special Interest"
+    assert club.valid?
+  end
+  test "Club should store values in the correct places" do
+    club = Club.new
+    club.name = "test club"
+    club.mission = "test mission"
+    club.link = "www.osu.edu"
+    club.location = "columbus"
+    club.affiliations = "Special Interest"
+    assert club.name == "test club"
+    assert club.mission == "test mission"
+    assert club.affiliations == "Special Interest"
+  end
 end
